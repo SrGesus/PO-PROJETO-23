dead5100405b4c1dbc14bc6053a490fc59f41c6f
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2023-10-10 05:34:45 +0100

    Removed DoShow FIXME comment

--- xxl-app/src/xxl/app/edit/DoShow.java
+++ xxl-app/src/xxl/app/edit/DoShow.java
@@ -18,7 +18,6 @@ class DoShow extends Command<Spreadsheet> {
 
     @Override
     protected final void execute() throws CommandException {
-        // FIXME implement command
         try {
             _receiver.showGama(stringField("gama")).forEach(str -> _display.popup(str));
         } catch (InvalidRangeException e) {
